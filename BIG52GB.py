88ed9723f19bd957d19afab7a3a2d3317e687e01
dongshuyan <sauterne@qq.com>
2020-07-03 21:18:24 +0800

    支持GBK编码

--- BIG52GB.py
+++ BIG52GB.py
@@ -4,14 +4,14 @@ import sys
 import re
 def simple2tradition(line):
     #将简体转换成繁体
-    line = Converter('zh-hant').convert(line.decode('utf-8'))
-    #line = line.encode('utf-8')
+    line = Converter('zh-hant').convert(line.encode('utf-8').decode('utf-8'))
+    line = line.encode('utf-8')
     return line
  
 def tradition2simple(line):
     # 将繁体转换成简体
-    line = Converter('zh-hans').convert(line.decode('utf-8'))
-    #line = line.encode('utf-8')
+    line = Converter('zh-hans').convert(line.encode('utf-8').decode('utf-8'))
+    line = line.encode('utf-8')
     return line
 
 def TraditionalToSimplified(line):          #繁体转简体
@@ -27,8 +27,14 @@ def SimplifiedToTraditional(line):          #简体转繁体
 
 
 def BIG52GB(path):
-    with open(path, 'r') as f1:
-        list1 = f1.readlines()
+    pan=0
+    try:
+        with open(path, 'r') as f1:
+            list1 = f1.readlines()
+    except:
+        pan=1
+        with open(path, "r", encoding='UTF-8') as f1:
+            list1 = f1.readlines()
     for i in range(len(list1)):
         list1[i]=TraditionalToSimplified(list1[i])
     newpath=''
@@ -36,14 +42,26 @@ def BIG52GB(path):
         newpath=newpath+"."+path[:path.find('.')]
         path=path[path.find('.')+1:]
     newpath=newpath[1:]+'.GB.'+path
-    f=open(newpath,'w')
+    if pan==0:
+        f=open(newpath,'w') 
+    else:
+        f=open(newpath,'wb') 
     for child in list1:
-        f.write(child)
+        if pan==0:
+            f.write(child)
+        else:
+            f.write(child.encode('UTF-8'))
     f.close()
 
 def GB2BIG5(path):
-    with open(path, 'r') as f1:
-        list1 = f1.readlines()
+    pan=0
+    try:
+        with open(path, 'r') as f1:
+            list1 = f1.readlines()
+    except:
+        pan=1
+        with open(path, "r", encoding='UTF-8') as f1:
+            list1 = f1.readlines()
     for i in range(len(list1)):
         list1[i]=SimplifiedToTraditional(list1[i])
     newpath=''
@@ -51,37 +69,57 @@ def GB2BIG5(path):
         newpath=newpath+"."+path[:path.find('.')]
         path=path[path.find('.')+1:]
     newpath=newpath[1:]+'.BIG5.'+path
-    f=open(newpath,'w')
+    if pan==0:
+        f=open(newpath,'w') 
+    else:
+        f=open(newpath,'wb') 
     for child in list1:
-        f.write(child)
+        if pan==0:
+            f.write(child)
+        else:
+            f.write(child.encode('UTF-8'))
     f.close()
 
-def BIG52GB_path(path,transfer):
+def BIG52GB_path(path,transfer,lan):
     files = os.listdir(path)
     for file_ in files:
         if os.path.isfile(path+file_):
-            if (file_.find('.ass') >=0 or file_.find('.srt') >=0 )and file_[0]!='.' :
+            name=file_
+            newname=''
+            while not(name.find('.')==-1):
+                newname=newname+"."+name[:name.find('.')]
+                name=name[name.find('.')+1:]
+            #if (file_.find('.ass') >=0 or file_.find('.srt') >=0 or file_.find('.ssa') >=0 or file_.find('.sub') >=0 or file_.find('.smi') >=0 ) and file_[0]!='.' :
+            if (name.find('ass') >=0 or name.find('srt') >=0 or name.find('ssa') >=0 or name.find('sub') >=0 or name.find('smi') >=0 ) and file_[0]!='.' :
                 print("file=",file_)
-                if (len(re.findall(r"简体",transfer)) >0 or len(re.findall(r"implified",transfer)) >0):
+                if (len(re.findall(r"S",transfer)) >0 or len(re.findall(r"J",transfer)) >0 or len(re.findall(r"简体",transfer)) >0 or len(re.findall(r"implified",transfer)) >0):
+                    if len(re.findall(r"CN",lan)) >0:
+                        print("转换为简体\n")
+                    else:
+                        print("Convert to Simplified\n")
                     BIG52GB(path+file_)
-                if (len(re.findall(r"繁体",transfer)) >0 or len(re.findall(r"raditional",transfer)) >0):
-                	GB2BIG5(path+file_)
+                if (len(re.findall(r"T",transfer)) >0 or len(re.findall(r"F",transfer)) >0 or len(re.findall(r"繁体",transfer)) >0 or len(re.findall(r"raditional",transfer)) >0):
+                    if len(re.findall(r"CN",lan)) >0:
+                        print("转换为繁体\n")
+                    else:
+                        print("Convert to Traditional\n")
+                    GB2BIG5(path+file_)
 if __name__ == '__main__':
     lan  = input("Please choose language: \n(中文请输入CN, EN for English.) \nExample:CN\n")
     if len(re.findall(r"CN",lan)) >0:
-        path= input("请输入字幕文件所在文件夹绝对路径.\n例如: /Users/moyu/Downloads/subtitle\n")
-        transfer=input("转换为简体还是繁体？\n例如:简体\n")
+        path= input("请输入字幕文件所在文件夹绝对路径.\n例如(Mac): /Users/moyu/Downloads/subtitle\n例如（Windows）: C:\\subtitle\n")
+        transfer=input("转换为简体(S)还是繁体(T)？\n例如:S\n")
     elif len(re.findall(r"EN",lan))>0:
-        path= input("Please enter the absolute path of the folder where the subtitle file is located\n例如: /Users/moyu/Downloads/subtitle\n")
-        transfer=input("Convert to Simplified or Traditional?\nExample:Simplified\n")
+        path= input("Please enter the absolute path of the folder where the subtitle file is located\nExample(Mac): /Users/moyu/Downloads/subtitle\nExample（Windows）: C:\\subtitle\n")
+        transfer=input("Convert to Simplified(S) or Traditional(T)?\nExample:S\n")
     else:
         print("Error, Thanks for using!")
         exit()
     if (path[len(path)-1]!='/'):
         path=path.strip()+"/"
     print("\n\n_____________START_____________")
-    BIG52GB_path(path,transfer)
-
+    BIG52GB_path(path,transfer,lan)
+    en=input("Complete Successfully！")
 
     '''
     path = sys.argv[1]
--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# Introduction
+# Introduction/介绍
 
 本工具旨在自动将字幕文件夹里面的所有字幕进行繁简切换。
-# Usage
+
+# Example/示例
+
+### 1.在视频文件夹中原有文件:
+* zh-Hant_01.srt
+* zh-Hant_02.srt
+
+### 2.运行脚本
+Windows用户可以直接运行可执行文件：字幕繁简互换工具_Windows版.exe
+
+#### Mac/Linux用户建议自己编译运行：
+python BIG52GB.py
+
+### 3.根据提示输入字幕文件夹的绝对路径以及转简体/繁体
+
+### 4.运行结束后，字幕文件夹中的字幕名变为(转简体)：
+* zh-Hant_01_GB.srt
+* zh-Hant_02_GB.srt
+字幕内容已经转换完毕
+
+
+# Usage/用法
 
 python3 BIG52GB.py
